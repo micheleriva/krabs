8e6381d93f0ced63af917b2da9e7f92d3710f996
Michele Riva <master@micheleriva.it>
2021-03-15 09:06:31 +0100

    feat(config): added support for krabs.config.js configuration file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,19 @@
+import { existsSync } from 'fs';
 import { join } from 'path';
 import { Config, ConfigEntry } from './config.d';
 
 export function getCwdConfig() {
-  const configFile = join(process.cwd(), '.krabs.js');
-  return require(configFile);
+  const validConfigNames = ['.krabs.js', 'krabs.config.js'];
+  
+  for (const name of validConfigNames) {
+    const configFile = join(process.cwd(), name);
+
+    if (existsSync(configFile)) {
+      return require(configFile);
+    }
+  }
+
+  throw Error(`No configuration file found`);
 }
 
 export async function getTenantConfig(conf?: ConfigEntry): Promise<Config> {
