884a13b2833be851241debc4e3bf3fe4255dee07
Michele Riva <master@micheleriva.it>
2021-05-28 21:35:22 +0200

    chore: fixed memoize import

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import * as chalk from 'chalk';
 import { parse } from 'url';
 import * as path from 'path';
-import memoize from 'fast-memoize';
+import { default as memoize } from 'fast-memoize';
 import { getTenantConfig } from './config';
 import { Config } from './config/config.d';
 import findTenant from './tenants/findTenant';
